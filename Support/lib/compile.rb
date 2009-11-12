b539a7a733a0a1818a83ca33e5b6887a6c157d62
Matt Kirman <mattkirman@redflex.co.uk>
2009-11-12 16:12:18 +0000

    Fixed 'no method' error. Again

--- Support/lib/compile.rb
+++ Support/lib/compile.rb
@@ -2,56 +2,56 @@
 
 begin
 	selected_files_string = ENV["TM_SELECTED_FILES"]
-rescue Exception => e
-	print "No files selected"
-	exit
-end
+	
+	# Strip the start and end quotes, then split the string into files
+	selected_files = selected_files_string[1..-2].split("' '")
+
+	if selected_files.length == 0 then
+		print "No files selected"
+		exit
+	end
 
 
-# Strip the start and end quotes, then split the string into files
-selected_files = selected_files_string[1..-2].split("' '")
-
-if selected_files.length == 0 then
-	print "No files selected"
-	exit
-end
-
-
-begin
-	compiler = ENV["TM_GOOGLE_CLOSURE_COMPILER_LOCATION"]
-rescue Exception => e
-	print "Unable to find path for your Google Closure Compiler in 'Preferences...'. Please refer to 'Help' in the bundle."
-	exit
-end
+	begin
+		compiler = ENV["TM_GOOGLE_CLOSURE_COMPILER_LOCATION"]
+	rescue Exception => e
+		print "Unable to find path for your Google Closure Compiler in 'Preferences...'. Please refer to 'Help' in the bundle."
+		exit
+	end
 
 
-level = ENV["TM_GOOGLE_CLOSURE_COMPILER_OPTIMIZATION"] || 'SIMPLE_OPTIMIZATION'
-compilation_level = ''
-if level == 'ADVANCED_OPTIMIZATIONS' then
-	compilation_level = ' --compilation_level ADVANCED_OPTIMIZATIONS'
-end
+	level = ENV["TM_GOOGLE_CLOSURE_COMPILER_OPTIMIZATION"] || 'SIMPLE_OPTIMIZATION'
+	compilation_level = ''
+	if level == 'ADVANCED_OPTIMIZATIONS' then
+		compilation_level = ' --compilation_level ADVANCED_OPTIMIZATIONS'
+	end
 
 
-files = ''
-selected_files.each { |f| files << " --js \"#{f}\"" }
-output_file = "\"#{ENV["TM_DIRECTORY"]}/compiled.js\""
-if selected_files.length == 1 then
-	file = selected_files[0].gsub('.js', '')
-	output_file = "\"#{file}-compiled.js\""
-end
-
-
-cmd = "java -jar #{compiler}#{compilation_level}#{files} --js_output_file #{output_file}"
-result = system(cmd)
-
-
-if result == true then
-	pluralisation = "s were"
+	files = ''
+	selected_files.each { |f| files << " --js \"#{f}\"" }
+	output_file = "\"#{ENV["TM_DIRECTORY"]}/compiled.js\""
 	if selected_files.length == 1 then
-		pluralisation = " was"
+		file = selected_files[0].gsub('.js', '')
+		output_file = "\"#{file}-compiled.js\""
+	end
+
+
+	cmd = "java -jar #{compiler}#{compilation_level}#{files} --js_output_file #{output_file}"
+	result = system(cmd)
+
+
+	if result == true then
+		pluralisation = "s were"
+		if selected_files.length == 1 then
+			pluralisation = " was"
+		end
+
+		print "Your file#{pluralisation} compiled successfully to: #{output_file}"
+	else
+		print result
 	end
 	
-	print "Your file#{pluralisation} compiled successfully to: #{output_file}"
-else
-	print result
+rescue Exception => e
+	print "No files selected"
+	exit
 end
